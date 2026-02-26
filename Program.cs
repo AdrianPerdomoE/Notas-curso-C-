b64334b22e6ebbe3d89ea3cbd1c3cce17719b2fb
AdrianPerdomoE <perdomoecheverri@gmail.com>
2026-02-26 09:28:20 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Notas_curso_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
